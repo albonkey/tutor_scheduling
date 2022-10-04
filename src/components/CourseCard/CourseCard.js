3f9fc044703128c54711e6302865e88ede47c79e
SOLEIGH <92748968+SOLEIGH@users.noreply.github.com>
2022-10-03 17:33:27 -0700

    ProfileCourse and CourseCard

---
+++ src/components/CourseCard/CourseCard.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import style from './CourseCard.module.scss';
+import StarRating from '../StarRating/StarRating';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown as downCaret} from '@fortawesome/free-solid-svg-icons';
+import { faCaretUp as upCaret} from '@fortawesome/free-solid-svg-icons';
+
+
+const CourseCard = ({courses}) => {
+    const [isSelected, setIsSelected] = useState(false);
+
+    return(
+        <div className={style.wrapper}>
+            <div className={style.intro}>
+                <div className={style.course}>
+                    <span className={style.heading3}>
+                        {courses.subject}
+                        <div className={style.subheading}>
+                            {courses.nrOfSessions} Sessions
+                        </div>
+                    </span>
+                </div>
+                <div className={style.icons}>
+                    <div>
+                        <StarRating rating={courses.rating} />
+                    </div>
+                    <div 
+                        className={style.carets} 
+                        onClick = {() => setIsSelected(!isSelected)}
+                    >
+                        {
+                            isSelected ?
+                            <div>
+                                <FontAwesomeIcon icon = {upCaret} />
+                            </div> 
+                            :
+                            <FontAwesomeIcon icon = {downCaret} />
+                        }
+                    </div>
+                </div>
+            </div>
+            <div className={[style.extended, !isSelected && style.hidden].join(' ')}> 
+                    <p className={style.info}>
+                        {courses.info}
+                    </p>
+            </div>
+          
+        </div>
+    )
+}
+
+export default CourseCard;
