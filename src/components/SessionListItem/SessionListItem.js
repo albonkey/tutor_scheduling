e1762e419aa9d967dedb0feca48f64b5268dff4b
SOLEIGH <92748968+SOLEIGH@users.noreply.github.com>
2022-10-11 18:46:10 -0700

    Fixed commented issues

--- src/components/SessionListItem/SessionListItem.js
+++ src/components/SessionListItem/SessionListItem.js
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import style from './SessionListItem.module.scss';
 
 const SessionListItem = ({session}) => {
-    const [isSelected, setIsSelected] = useState(false);
 
 const background = session.isTeaching ? style.teach : style.learn;
 
     return(
-        <div className = {style.wrapper + ' ' + background}> 
+        <div className = {style.wrapper + ' ' + background}>
             <div className={style.info}>
                 <span className={style.status}>
                     {
@@ -17,9 +16,7 @@ const background = session.isTeaching ? style.teach : style.learn;
                 <span>{`${session.subject} with ${session.name}`}</span>
             </div>
             <div className={style.info}>
-                <a className={style.link} 
-                    onClick={() => setIsSelected(!isSelected)}
-                    >View Session</a>
+                <a className={style.link}>View Session</a>
                 <span>{session.time}</span>
             </div>
         </div>
