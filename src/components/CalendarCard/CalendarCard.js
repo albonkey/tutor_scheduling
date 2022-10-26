4e4a6acb56f76721f39ec1a3245f9ce06f7927b9
SOLEIGH <92748968+SOLEIGH@users.noreply.github.com>
2022-10-25 21:37:52 -0700

    formatting

--- src/components/CalendarCard/CalendarCard.js
+++ src/components/CalendarCard/CalendarCard.js
@@ -3,27 +3,27 @@ import style from './CalendarCard.module.scss';
 
 const CalendarCard = ({date, details}) => {
     return(
-          <div className={style.card}>
+        <div className={style.card}>
             <div className = {style.header}>
                 <div className={style.date}>
                     {date}
                 </div>
-            <div>
-                <div className = {style.cardbody}>
-                {
-                    details.map(detail=> {
-                        return(
-                            detail.isTeaching ?
-                                 <div className = {style.day}>{detail.time} Teaching {detail.subject} with {detail.name}</div>
-                            :
-                                <div className = {style.day}>{detail.time} Session with {detail.name}</div>
-                        )
-                    })
-                }
-                </div>
-                </div>
+                <div>
+                    <div className = {style.cardbody}>
+                    {
+                        details.map(detail=> {
+                            return(
+                                detail.isTeaching ?
+                                    <div className = {style.day}>{detail.time} Teaching {detail.subject} with {detail.name}</div>
+                                :
+                                    <div className = {style.day}>{detail.time} Session with {detail.name}</div>
+                            )
+                        })
+                    }
+                    </div>
                 </div>
             </div>
+        </div>
     )
 }
 
