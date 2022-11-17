c851ebe436cfb809727c32374c43937b042abd5c
Aylin <aylin.msvn@gmail.com>
2022-11-16 16:06:10 -0800

    Update Sessions

--- amplify/backend/function/SessionsEndpoint/src/app.js
+++ amplify/backend/function/SessionsEndpoint/src/app.js
@@ -1,11 +1,3 @@
-/*
-Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-    http://aws.amazon.com/apache2.0/
-or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and limitations under the License.
-*/
-
 const express = require('express')
 const bodyParser = require('body-parser')
 //const {randomUUID} = require('crypto');
@@ -49,11 +41,10 @@ app.use(function(req, res, next) {
     res.status(500).json({err:err});
   }
 });
-  
+
 /****************************
 * Get a session details by id *
 ****************************/
-
 app.get('/sessions/:id', async(req, res) => {
   const {id} = req.params;
 
@@ -69,13 +60,12 @@ app.get('/sessions/:id', async(req, res) => {
 
   try {
     const data = await docClient.query(params).promise();
-    
+
     res.json({success: 'get call succeed!', data: data});
   } catch (err) {
     res.status(500).json({err:err});
   }
 });
-
 
 /****************************
 * Get session by subject *
@@ -129,13 +119,12 @@ app.delete('/sessions/:id', async(req, res) => {
   }
 });
 
+
 app.listen(3000, function() {
-  console.log("App started")
+    console.log("App started")
 });
 
 // Export the app object. When executing the application local this does nothing. However,
 // to port it to AWS Lambda we will create a wrapper around that will load the app from
 // this file
 module.exports = app
-
-
